c6e1424554339900ca12e2a9d3f96cae2a6b6699
RHsanto <rakibulsanto789@gmail.com>
2022-04-12 12:53:53 +0600

    add successfully slider

--- src/components/Home/Instructor/Instructor.js
+++ src/components/Home/Instructor/Instructor.js
@@ -8,13 +8,24 @@ import { AiFillInstagram } from "react-icons/ai";
 import { BsYoutube } from "react-icons/bs";
 import Slider from "react-slick";
 const Instructor = () => {
+
+  // var settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   autoplaySpeed: 2000,
+  //   pauseOnHover: true,
+    
+  // };
   var settings = {
     dots: true,
     infinite: true,
+    speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplaySpeed: 2000,
-    pauseOnHover: true
+    pauseOnHover: true,
   };
   return (
     <div className='instructor-section'>
@@ -24,10 +35,10 @@ const Instructor = () => {
            <div className="title">
              <h2 className='fw-bold'>Our Expert Instructor</h2>
            </div>
-           <div className="d-flex gap-3 arrow">
+           {/* <div className="d-flex gap-3 arrow">
              <div><IoIosArrowBack/></div>
              <div><IoIosArrowForward/></div>
-           </div>
+           </div> */}
         </div>
         <div className='my-5 gap-4'>
         <Slider {...settings}>
