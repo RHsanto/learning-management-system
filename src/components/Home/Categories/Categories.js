9036dff594cc97c125d648d1d7f454f96687f86d
RHsanto <rakibulsanto789@gmail.com>
2022-04-13 23:32:05 +0600

    add all css

--- src/components/Home/Categories/Categories.js
+++ src/components/Home/Categories/Categories.js
@@ -15,49 +15,49 @@ const Categories = () => {
      <div className="container categories">
        <div className="row text-center mx-2 g-4">
          <h2 className=' fw-bold mb-5'>Top Categories</h2>
-         <div className=" col-lg-3 category">
+         <div className="col-lg-3 col-md-6 col-12 category">
            <div className="d-flex ">
              <div className="c-icons"><FaPen/></div>
              <h5>Design Deve</h5>
            </div>
          </div>
-         <div className="col-lg-3 category">
+         <div className="col-lg-3 col-md-6 col-12 category">
          <div className="d-flex">
              <div className="c-icons"> <AiOutlineAppstore/></div>
              <h5>Development</h5>
            </div>
          </div>
-         <div className="col-lg-3 category">
+         <div className="col-lg-3 col-md-6 col-12 category">
          <div className="d-flex">
              <div className="c-icons"> <FaMicrophoneAlt/></div>
              <h5>Marketing</h5>
            </div>
          </div>
-         <div className="col-lg-3 category">
+         <div className="col-lg-3 col-md-6 col-12 category">
          <div className="d-flex">
              <div className="c-icons"> <SiPaloaltosoftware/></div>
              <h5>It & Software</h5>
            </div>
          </div>
-         <div className="col-lg-3 category">
+         <div className="col-lg-3 col-md-6 col-12 category">
          <div className="d-flex">
              <div className="c-icons"> <BsFillCameraVideoFill/></div>
              <h5>Videography</h5>
            </div>
          </div>
-         <div className="col-lg-3 category">
+         <div className="col-lg-3 col-md-6 col-12 category">
          <div className="d-flex">
              <div className="c-icons"> <BsHandbagFill/></div>
              <h5>Business Sys</h5>
            </div>
          </div>
-         <div className="col-lg-3 category">
+         <div className="col-lg-3 col-md-6 col-12 category">
          <div className="d-flex">
              <div className="c-icons"><TiCameraOutline/></div>
              <h5>Photography</h5>
            </div>
          </div>
-         <div className="col-lg-3 category">
+         <div className="col-lg-3 col-md-6 col-12 category">
          <div className="d-flex">
              <div className="c-icons"><MdQueueMusic/></div>
              <h5>Musical Intru</h5>
--- src/components/Home/Offer/Offer.js
+++ src/components/Home/Offer/Offer.js
@@ -13,7 +13,7 @@ const Offer = () => {
         <p className='sub-title fw-bold'>What We Offer</p>
          <h2 className='fw-bold'>For Your Future Learning.</h2>
         </div>
-         <div className="col-lg-3">
+         <div className="col-lg-3 col-md-6 col-12">
          <div className="l-card  p-4 rounded bg-light">
          <li className='l-card-icon'> <p className='icons'><FaRegPaperPlane /></p></li>
          <h5 className='fw-bold'>Online Courses</h5>
@@ -21,7 +21,7 @@ const Offer = () => {
             for unique catalysts for change my ocardinat.</p>
          </div>
          </div>
-         <div className="col-lg-3">
+         <div className="col-lg-3 col-md-6 col-12">
          <div className="l-card  p-4 rounded bg-light">
            <li className='l-card-icon'> <p className='icons'><AiOutlineUser /></p></li>
          <h5 className='fw-bold'>Expert Trainer</h5>
@@ -29,7 +29,7 @@ const Offer = () => {
             for unique catalysts for change my ocardinat.</p>
          </div>
          </div>
-         <div className="col-lg-3">
+         <div className="col-lg-3 col-md-6 col-12">
          <div className="l-card  p-4 rounded bg-light">
          <li className='l-card-icon'> <p className='icons'><CgFileDocument /></p></li>
          <h5 className='fw-bold'>Get Certificate</h5>
@@ -37,7 +37,7 @@ const Offer = () => {
             for unique catalysts for change my ocardinat.</p>
          </div>
          </div>
-         <div className="col-lg-3">
+         <div className="col-lg-3 col-md-6 col-12">
          <div className="l-card  p-4 rounded bg-light">
          <li className='l-card-icon'> <p className='icons'><MdOutlineDateRange /></p></li>
          <h5 className='fw-bold'>Life Time Access</h5>
