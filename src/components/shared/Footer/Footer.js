83cd8ff2c1347de85d763928b41cb28d440c3e2a
RHsanto <rakibulsanto789@gmail.com>
2022-04-13 14:02:29 +0600

    add footer

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -1,11 +1,66 @@
 import React from 'react';
 import './Footer.css'
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { AiOutlineYoutube } from "react-icons/ai";
+import { RiBasketballLine } from "react-icons/ri";
+import { FaWhatsapp } from "react-icons/fa";
 const Footer = () => {
   return (
     <div >
       <footer>
-
-      <h1>Footer</h1>
+       <div className="container fot">
+         <div className="row">
+           <div className="col-lg-3">
+            <div className="fot-1">
+            <h5>About</h5>
+               <li>About us</li>
+               <li>Blog</li>
+               <li>Careers</li>
+               <li>Jobs</li>
+               <li>In Press</li>
+            </div>
+           </div>
+           <div className="col-lg-3">
+           <div className="fot-2">
+            <h5>Quick Links</h5>
+               <li>Refund Policy</li>
+               <li>Documentation</li>
+               <li>Chat online</li>
+               <li>Order Cancel</li>
+               <li>Privacy Policy</li>
+            </div>
+           </div>
+           <div className="col-lg-2">
+           <div className="fot-3">
+            <h5>Support</h5>
+               <li>Contact us</li>
+               <li>Online Chat</li>
+               <li>Whatsapp</li>
+               <li>Telegram</li>
+               <li>Ticketing</li>
+            </div>
+           </div>
+           <div className="col-lg-4">
+           <div className="fot-4">
+            <h5>Subscribe For Newsletter</h5>
+            <p>Be the first one to know about discounts,offers and events.
+             Unsubscribe whenever you like.</p>
+            <div className="fot-search d-flex">
+            <input type="email" name="" id="" placeholder='&#9993; Enter your email' />
+            <button type="submit">Subscribe Now</button>
+            </div>
+              <div className="fot-icon d-flex">
+              <li><RiFacebookCircleLine/></li>
+               <li><AiOutlineYoutube/></li>
+               <li><RiBasketballLine/></li>
+               <li><FaWhatsapp/></li>
+              </div>
+            </div>
+           </div>
+         </div>
+       </div>
+  
+     
       </footer>
     </div>
   );
