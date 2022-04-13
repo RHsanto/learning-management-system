c2a1ebd89cfaad29a5378e7b7df0adcec1a338d7
RHsanto <rakibulsanto789@gmail.com>
2022-04-13 17:38:38 +0600

    add hero css

--- src/components/Home/HeroSection/HeroSection.js
+++ src/components/Home/HeroSection/HeroSection.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from '../../shared/Navbar/Navbar'
 import './Herosection.css'
-import Hero from '../../../image/hero-women.png'
+import Hero from '../../../image/sada-removebg-preview.png'
 const HeroSection = () => {
   return (
     <div className='hero-section'>
       <Navbar/>
-     <div className="container">
+     <div className="container ">
       <div className="row banner">
         <div className="col-lg-6 banner-info">
           <p>Learn New Skills & Popular Courses</p>
@@ -15,8 +15,20 @@ const HeroSection = () => {
             "organic" sources. Competently leverage other.</small></p>
             <button className=" global-btn">Explore Courses</button>
         </div>
-        <div className="col-lg-4 hero-img">
+        <div className="col-lg-6 hero-img ">
           <img src={Hero} alt="" />
+          <div className="text-1">
+            <h5 className='sub-title'>235K</h5>
+            <p><small>Worldwide Students</small></p>
+          </div>
+          <div className="text-2">
+            <h5 >4.7</h5>
+            <p><small>Worldwide Students</small></p>
+          </div>
+          <div className="text-3">
+            <h5>3.5K</h5>
+            <p><small>Free Pro Courses</small></p>
+          </div>
         </div>
       </div>
      </div>
