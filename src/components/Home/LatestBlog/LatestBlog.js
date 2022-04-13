8cf9ba57801aa4f57a31b918710470c232127d8d
RHsanto <rakibulsanto789@gmail.com>
2022-04-13 11:55:48 +0600

    add

--- src/components/Home/LatestBlog/LatestBlog.js
+++ src/components/Home/LatestBlog/LatestBlog.js
@@ -8,7 +8,7 @@ const LatestBlog = () => {
          <h2 className='text-center blogs-title'><b>Latest Blogs & News</b></h2>
         <div className="col-lg-4">
          <div class="card" >
-         <img src="https://media.istockphoto.com/photos/elearning-education-concept-learning-online-picture-id1290864946?b=1&k=20&m=1290864946&s=170667a&w=0&h=zZq7rG5McSptSIpEm9f8iTGd3Mrdkcslakr91T7qTYM=" class="card-img-top" alt="..."/>
+         <img src="https://images.unsplash.com/photo-1628243426757-b092ba839ff9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
          <div class="card-body">
           <p className='sub-title m-0 pb-1'><small>EDUCATION- JAN 21,2022</small></p>
          <h5 class="latest-title fw-bold">Educational Technology & Mobile Learning</h5>
@@ -19,7 +19,7 @@ const LatestBlog = () => {
         </div>
         <div className="col-lg-4">
          <div class="card" >
-         <img src="https://media.istockphoto.com/photos/elearning-education-concept-learning-online-picture-id1290864946?b=1&k=20&m=1290864946&s=170667a&w=0&h=zZq7rG5McSptSIpEm9f8iTGd3Mrdkcslakr91T7qTYM=" class="card-img-top" alt="..."/>
+         <img src="https://images.unsplash.com/photo-1577896851231-70ef18881754?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="..."/>
          <div class="card-body">
           <p className='sub-title m-0 pb-1'><small>EDUCATION- JAN 21,2022</small></p>
          <h5 class="latest-title fw-bold">Educational Technology & Mobile Learning</h5>
@@ -30,7 +30,7 @@ const LatestBlog = () => {
         </div>
         <div className="col-lg-4">
          <div class="card" >
-         <img src="https://media.istockphoto.com/photos/elearning-education-concept-learning-online-picture-id1290864946?b=1&k=20&m=1290864946&s=170667a&w=0&h=zZq7rG5McSptSIpEm9f8iTGd3Mrdkcslakr91T7qTYM=" class="card-img-top" alt="..."/>
+         <img src="https://images.unsplash.com/photo-1636202339022-7d67f7447e3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
          <div class="card-body">
           <p className='sub-title m-0 pb-1'><small>EDUCATION- JAN 21,2022</small></p>
          <h5 class="latest-title fw-bold">Educational Technology & Mobile Learning</h5>
