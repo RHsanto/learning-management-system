6f00263bebb77fc4025f180464374ea7f66e2363
RHsanto <rakibulsanto789@gmail.com>
2022-04-25 01:00:47 +0600

    [add firebase]

---
+++ src/components/Firebase/Firebase-config.js
@@ -0,0 +1,13 @@
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxfHOcJS_UAY3Vo04nRH0nmIumXHb7LlA",
+  authDomain: "e-learning-9faa2.firebaseapp.com",
+  projectId: "e-learning-9faa2",
+  storageBucket: "e-learning-9faa2.appspot.com",
+  messagingSenderId: "795127428670",
+  appId: "1:795127428670:web:7555a85bdba5f40294036f"
+};
+
+export default firebaseConfig;
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { getAuth, GoogleAuthProvider , onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import initAuthentication from '../components/Firebase/Firebase-init';
+
+initAuthentication();
+
+
+const useFirebase = () => {
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+const [user,setUser]=useState({})
+const [error,setError] = useState("")
+
+
+const signInUsingGoogle =()=>{
+    signInWithPopup(auth,provider)
+    .then(result=>{
+      setUser(result.user)
+    })
+   .catch(error => {
+     setError(error.message);
+     });
+ }
+ 
+ const logOut = ()=>{
+   signOut(auth)
+   .then(()=>{
+     setUser({})
+   })
+ }
+
+ useEffect(()=>{
+  const unsubscribe =onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user)
+      } 
+    });
+    return unsubscribe;
+  },[auth])
+
+
+  return{
+signInUsingGoogle,
+logOut,
+user,error
+  };
+};
+
+export default useFirebase;
