6b5500d203fccbdc7a6ba03be7b728f0e2018049
RHsanto <rakibulsanto789@gmail.com>
2022-11-05 21:51:51 +0600

    change api

--- src/components/Home/LatestBlog/LatestBlog.js
+++ src/components/Home/LatestBlog/LatestBlog.js
@@ -1,42 +1,47 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import './LatestBlog.css'
+import "./LatestBlog.css";
 const LatestBlog = () => {
-  const [blogs,setBlogs]=useState([]);
-  useEffect(()=>{
-    fetch('https://secure-sea-90788.herokuapp.com/all-blogs')
-    .then(response=>response.json())
-    .then(data=>setBlogs(data.slice(0,3)))
-  },[])
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("https://secure-sea-90788.herokuapp.com/all-blogs")
+      .then(response => response.json())
+      .then(data => setBlogs(data.slice(0, 3)));
+  }, []);
   return (
-    <div className='latest-section'>
-     <div className="container">
-       <div className="row g-4">
-         <h2 className='text-center blogs-title'><b>Latest Blogs & News</b></h2>
-       {blogs?.map(blog=>(
-          <div key={blog?._id} className="col-lg-4 col-md-6 col-12">
-          <div className="card shadow" >
-          <img src={blog?.img} className="card-img-top" alt="..."/>
-          <div className="card-body">
-           <p className='sub-title m-0 pb-1'><small>EDUCATION- JAN 21,2022</small></p>
-          <h5 className="latest-title fw-bold">Educational Technology & Mobile Learning</h5>
-          <p className="latest-text">Dramatically supply transparent deliverable before & you backward comp internal.</p>
-          </div>
+    <div className="latest-section">
+      <div className="container">
+        <div className="row g-4">
+          <h2 className="text-center blogs-title">
+            <b>Latest Blogs & News</b>
+          </h2>
+          {blogs?.map(blog => (
+            <div key={blog?._id} className="col-lg-4 col-md-6 col-12">
+              <div className="card shadow">
+                <img src={blog?.img} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <p className="sub-title m-0 pb-1">
+                    <small>EDUCATION- JAN 21,2022</small>
+                  </p>
+                  <h5 className="latest-title fw-bold">Educational Technology & Mobile Learning</h5>
+                  <p className="latest-text">
+                    Dramatically supply transparent deliverable before & you backward comp internal.
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
- 
-         </div>
-       ))}
-       </div>
         <div className="row">
           <div className="col-lg-2 mx-auto">
-          <button className='all-courses'>
-            <Link to='/news-blogs'>Load More</Link>
-          </button>
+            <button className="all-courses">
+              <Link to="/news-blogs">Load More</Link>
+            </button>
           </div>
         </div>
-     </div>
+      </div>
     </div>
   );
 };
--- src/components/Home/LatestBlog/LatestNews.js
+++ src/components/Home/LatestBlog/LatestNews.js
@@ -1,34 +1,39 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import './LatestBlog.css'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import "./LatestBlog.css";
 const LatestNews = () => {
-  const [blogs,setBlogs]=useState([]);
-  useEffect(()=>{
-    fetch('https://secure-sea-90788.herokuapp.com/all-blogs')
-    .then(response=>response.json())
-    .then(data=>setBlogs(data))
-  },[])
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("https://secure-sea-90788.herokuapp.com/all-blogs")
+      .then(response => response.json())
+      .then(data => setBlogs(data));
+  }, []);
   return (
-    <div className='latest-section'>
-     <div className="container">
-       <div className="row g-4">
-         <h2 className='text-center blogs-title'><b>Latest Blogs & News</b></h2>
-       {blogs?.map(blog=>(
-          <div key={blog?._id} className="col-lg-4 col-md-6 col-12">
-          <div className="card shadow" >
-          <img src={blog?.img} className="card-img-top" alt="..."/>
-          <div className="card-body">
-           <p className='sub-title m-0 pb-1'><small>EDUCATION- JAN 21,2022</small></p>
-          <h5 className="latest-title fw-bold">Educational Technology & Mobile Learning</h5>
-          <p className="latest-text">Dramatically supply transparent deliverable before & you backward comp internal.</p>
-          </div>
+    <div className="latest-section">
+      <div className="container">
+        <div className="row g-4">
+          <h2 className="text-center blogs-title">
+            <b>Latest Blogs & News</b>
+          </h2>
+          {blogs?.map(blog => (
+            <div key={blog?._id} className="col-lg-4 col-md-6 col-12">
+              <div className="card shadow">
+                <img src={blog?.img} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <p className="sub-title m-0 pb-1">
+                    <small>EDUCATION- JAN 21,2022</small>
+                  </p>
+                  <h5 className="latest-title fw-bold">Educational Technology & Mobile Learning</h5>
+                  <p className="latest-text">
+                    Dramatically supply transparent deliverable before & you backward comp internal.
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
- 
-         </div>
-       ))}
-       </div>
-     </div>
+      </div>
     </div>
   );
 };
