1d7788199b0b1a303208dc4a1863e7ba0bc31cdb
RHsanto <rakibulsanto789@gmail.com>
2022-11-05 21:53:21 +0600

    change api

--- src/components/Home/Categories/Categories.js
+++ src/components/Home/Categories/Categories.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Categories.css'
+import React from "react";
+import "./Categories.css";
 import { FaPen } from "react-icons/fa";
 import { AiOutlineAppstore } from "react-icons/ai";
 import { FaMicrophoneAlt } from "react-icons/fa";
@@ -11,60 +11,81 @@ import { MdQueueMusic } from "react-icons/md";
 
 const Categories = () => {
   return (
-    <div className='categories-section'>
-     <div className="container categories">
-       <div className="row text-center mx-2 g-4">
-         <h2 className=' fw-bold mb-5'>Top Categories</h2>
-         <div className="col-lg-3 col-md-6 col-12 category">
-           <div className="d-flex ">
-             <div className="c-icons"><FaPen/></div>
-             <h5>Design Deve</h5>
-           </div>
-         </div>
-         <div className="col-lg-3 col-md-6 col-12 category">
-         <div className="d-flex">
-             <div className="c-icons"> <AiOutlineAppstore/></div>
-             <h5>Development</h5>
-           </div>
-         </div>
-         <div className="col-lg-3 col-md-6 col-12 category">
-         <div className="d-flex">
-             <div className="c-icons"> <FaMicrophoneAlt/></div>
-             <h5>Marketing</h5>
-           </div>
-         </div>
-         <div className="col-lg-3 col-md-6 col-12 category">
-         <div className="d-flex">
-             <div className="c-icons"> <SiPaloaltosoftware/></div>
-             <h5>It & Software</h5>
-           </div>
-         </div>
-         <div className="col-lg-3 col-md-6 col-12 category">
-         <div className="d-flex">
-             <div className="c-icons"> <BsFillCameraVideoFill/></div>
-             <h5>Videography</h5>
-           </div>
-         </div>
-         <div className="col-lg-3 col-md-6 col-12 category">
-         <div className="d-flex">
-             <div className="c-icons"> <BsHandbagFill/></div>
-             <h5>Business Sys</h5>
-           </div>
-         </div>
-         <div className="col-lg-3 col-md-6 col-12 category">
-         <div className="d-flex">
-             <div className="c-icons"><TiCameraOutline/></div>
-             <h5>Photography</h5>
-           </div>
-         </div>
-         <div className="col-lg-3 col-md-6 col-12 category">
-         <div className="d-flex">
-             <div className="c-icons"><MdQueueMusic/></div>
-             <h5>Musical Intru</h5>
-           </div>
-         </div>
-       </div>
-     </div>
+    <div className="categories-section">
+      <div className="container categories">
+        <div className="row text-center mx-2 g-4">
+          <h2 className=" fw-bold mb-5">Top Categories</h2>
+          <div className="col-lg-3 col-md-6 col-12 category">
+            <div className="d-flex ">
+              <div className="c-icons">
+                <FaPen />
+              </div>
+              <h5>Design Deve</h5>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 col-12 category">
+            <div className="d-flex">
+              <div className="c-icons">
+                {" "}
+                <AiOutlineAppstore />
+              </div>
+              <h5>Development</h5>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 col-12 category">
+            <div className="d-flex">
+              <div className="c-icons">
+                {" "}
+                <FaMicrophoneAlt />
+              </div>
+              <h5>Marketing</h5>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 col-12 category">
+            <div className="d-flex">
+              <div className="c-icons">
+                {" "}
+                <SiPaloaltosoftware />
+              </div>
+              <h5>It & Software</h5>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 col-12 category">
+            <div className="d-flex">
+              <div className="c-icons">
+                {" "}
+                <BsFillCameraVideoFill />
+              </div>
+              <h5>Videography</h5>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 col-12 category">
+            <div className="d-flex">
+              <div className="c-icons">
+                {" "}
+                <BsHandbagFill />
+              </div>
+              <h5>Business Sys</h5>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 col-12 category">
+            <div className="d-flex">
+              <div className="c-icons">
+                <TiCameraOutline />
+              </div>
+              <h5>Photography</h5>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 col-12 category">
+            <div className="d-flex">
+              <div className="c-icons">
+                <MdQueueMusic />
+              </div>
+              <h5>Musical Intru</h5>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
