75a77d8af0f60ad92554ffe49b2c032f3b46e240
RHsanto <rakibulsanto789@gmail.com>
2022-11-05 21:53:36 +0600

    change api

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import './About.css'
-import MiddleImg from '../../../image/jakra.png'
-import LittleImg from '../../../image/cut.PNG'
-import headeMen from '../../../image/heade-man.avif'
+import React from "react";
+import "./About.css";
+import MiddleImg from "../../../image/jakra.png";
+import LittleImg from "../../../image/cut.PNG";
+import headeMen from "../../../image/heade-man.avif";
 import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div className='about-section'>
+    <div className="about-section">
       <div className="container">
         <div className="row about">
           <div className="col-lg-6 all-img">
-           <div className="headphone">
-           <img src={headeMen}
-            alt="" />
-           </div>
+            <div className="headphone">
+              <img src={headeMen} alt="" />
+            </div>
             <div className="mid-women">
               <img src={MiddleImg} alt="" srcSet="" />
             </div>
@@ -24,13 +23,17 @@ const About = () => {
           </div>
           <div className="col-lg-4">
             <div className="about-info">
-              <p className='sub-title fw-bold'>About Our Courses</p>
-              <h2 className='fw-bold'>Explore Thousands of Creative Classes</h2>
-              <p className='my-4 '><small>Dramatically supply transparent deliverables beforese backward comp internal
-               or "organic" sources. Comp transparent leverage other. </small></p>
-               <button className='global-btn'> 
-         <Link to='/popular-course'>Explore Courses</Link>
-               </button>
+              <p className="sub-title fw-bold">About Our Courses</p>
+              <h2 className="fw-bold">Explore Thousands of Creative Classes</h2>
+              <p className="my-4 ">
+                <small>
+                  Dramatically supply transparent deliverables beforese backward comp internal or
+                  "organic" sources. Comp transparent leverage other.{" "}
+                </small>
+              </p>
+              <button className="global-btn">
+                <Link to="/popular-course">Explore Courses</Link>
+              </button>
             </div>
           </div>
         </div>
